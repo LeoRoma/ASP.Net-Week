1deed392a916845e2e0f5bec93b9836a3a29b99c
LeoRoma <xiajtn@gmail.com>
2020-08-19 12:02:59 +0100

    Added lab_44 and 45

---
+++ lab_44_asp_framework_demo/lab_45_asp_core_demo/Pages/Leo.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab_45_asp_core_demo.Pages
+{
+    public class LeoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
