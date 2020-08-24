4cd789d3224a8325c50533dfb165f62bee2554b3
LeoRoma <xiajtn@gmail.com>
2020-08-24 15:33:09 +0100

    deployed on Azure

---
+++ lab_44_asp_framework_demo/lab_48_mvc_northwind/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lab_48_mvc_northwind.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
